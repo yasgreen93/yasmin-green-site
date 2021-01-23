0a8b31a4734a4d8471b6e338226a4072faf341ac
yasgreen93 <yasmin.green93@gmail.com>
2021-01-23 23:03:06 +0000

    Adding favicons and headers

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,19 +1,43 @@
 import path from 'path'
 
+const title = 'Yasmin Green | Developer'
+const description = 'Yasmin Green - Lead developer working with Javascript, HTML, CSS, NodeJS, NuxtJS, VueJS, ReactJS, PHP, Laravel and more, building a variety of websites.'
+
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'portfolio-site-nuxt',
+    title: 'Yasmin Green | Developer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { property: 'og:locale', content: 'en_GB' },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:title', content: title },
+      { property: 'og:description', content: description },
+      { property: 'og:image', content: '/profile-pic.jpg' },
+      { property: 'og:image:type', content: 'image/jpeg' },
+      { property: 'og:image:width', content: '400' },
+      { property: 'og:image:height', content: '400' },
+      { property: 'og:url', content: title },
+      { property: 'og:site_name', content: title },
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'twitter:description', content: description },
+      { name: 'twitter:title', content: title },
+      { name: 'twitter:site', content: '@yasgreen93' },
+      { name: 'twitter:creator', content: '@yasgreen93' },
+      { name: 'twitter:image', content: '/profile-pic.jpg' },
+      { hid: 'description', name: 'description', content: description }
     ],
     link: [
+      { rel: 'canonical', href: 'https://www.yasmingreen.com' },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Dosis:wght@300;500&display=swap' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Bodoni+Moda:wght@400;700&display=swap' }
