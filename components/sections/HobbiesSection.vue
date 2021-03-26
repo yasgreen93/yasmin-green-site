0e4eaf7ce3608934d0b35aff432946bca86f6439
yasgreen93 <yasmin.green93@gmail.com>
2021-03-26 21:39:23 +0000

    Adding new sections

---
+++ components/sections/HobbiesSection.vue
@@ -0,0 +1,38 @@
+<template>
+  <section
+    id="hobbies"
+    ref="hobbies"
+    class="Hobbies section"
+  >
+    <div class="wrapper">
+      <h2 class="t-h2 mb-b54 mt-b36 text-center">
+        Hobbies
+      </h2>
+      <ul>
+        <li v-for="(hobby, index) in hobbies" :key="index">
+          {{ hobby }}
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'HobbiesSection',
+  data: () => ({
+    hobbies: [
+      'Knitting',
+      'Reading',
+      'Skateboarding',
+      'Bouldering',
+      'Snowboarding',
+      'Skiing',
+      'Guitar',
+      'Piano'
+    ]
+  })
+}
+</script>
+
+<style scoped lang="postcss" />
